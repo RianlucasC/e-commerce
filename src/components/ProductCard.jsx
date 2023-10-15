6c737619bb74ab24f8a110480e7d63a66a59673b
Rianlucas7 <rianlucas592@gmail.com>
2023-10-15 16:48:01 -0300

    feat: productCard

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom"
+
+const ProductCard = ({_id: id, name, price, images}) => {
+
+  return (
+    <Link to={`/products/${id}`}>
+        <div className="flex flex-col items-start gap-2 px-2 w-[140px] h-[210px] m-6 shadow-md rounded-md  hover:border-[#2D80CC] hover:border text-center">
+        <img src={images[0]} alt="" className="w-[120px] h-[120px]"/>
+            <p className="font-bold text-sm ">R$: {price}</p>
+            <p className="font-normal text-sm text-[#737070]">{name}</p>
+        </div>
+    </Link>
+  )
+}
+
+export default ProductCard
